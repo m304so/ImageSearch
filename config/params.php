06029479e3ac5cadc4a31b4c632e899be803b638
Sergey Medvedev <m304so@gmail.com>
2016-11-08 22:44:06 +0400

    Update params.php

--- config/params.php
+++ config/params.php
@@ -2,9 +2,9 @@
 
 return [
 	'CountImages'	=> 10,
-	'GAppName'		=> 'Google Image Search',
-	'GClientID'		=> '[Your Google Client Id]',
+	'GAppName'	=> 'Google Image Search',
+	'GClientID'	=> '[Your Google Client Id]',
 	'GClientSecret'	=> '[Your Google Client Secret]',
-	'GEngineID'		=> '[Your Google Engine ID]',
-	'GApiKey'		=> '[Your Google Api Key]'
+	'GEngineID'	=> '[Your Google Engine ID]',
+	'GApiKey'	=> '[Your Google Api Key]'
 ];
